12fc81dcd8251edbe8be629853f550543c68a0b6
KeshavJ1007 <keshavjai2006@gmail.com>
2025-11-14 15:58:26 +0530

    Add files via upload

---
+++ dfom.v
@@ -0,0 +1,10 @@
+module dfom(A,B,C,D,S1,S0,Y);
+    input A,B,C,D,S1,S0;
+    output Y;
+    
+    assign Y =   (S1 == 0 && S0 == 0) ? A:
+                 (S1 == 0 && S0 == 1) ? B:
+                 (S1 == 1 && S0 == 0) ? C: 
+                 (S1 == 1 && S0 == 1) ? D: 1'b0;
+                                         
+endmodule
